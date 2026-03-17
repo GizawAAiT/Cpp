64000a6097f0b98720368f953dfacf4a5e8ffc60
GizawAAiT <gizawdagnemogessie@gmail.com>
2026-03-17 13:04:22 +0300

    first useless program:

--- Topics/basics/main.cpp
+++ Topics/basics/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
-int main() 
+# include <iostream>
+#include <vector>
+int square(int x)
 {
-    std::cout << "Test";
+    return x * x;
+}
+
+
+int main()
+{
+    int x = 3;
+    int res = square(x);
+    std::cout << "The square of " << x << " is "<< res << "\n";
+    
+    // vector:
+    std::vector<int> v = {1,2,3,4,5};
+    for (const auto& elem: v)
+    {
+        std::cout << elem << " ";
+    }
+    // std::cout << "vector: " <<v;
+    
+    return 0;
 }
