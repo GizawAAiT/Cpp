41ae5502e860961883713247fbd3bce9416faf45
GizawAAiT <gizawdagnemogessie@gmail.com>
2026-03-26 12:32:08 +0300

    namespace

---
+++ Topics/basics/_namespace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+namespace number_space 
+{
+    float my_pi = 3.0014;
+}
+
+using namespace number_space;
+using namespace std;
+
+int main()
+{
+
+    cout << "The value of pi is: " << my_pi << endl;
+    
+    return 0;
+}
